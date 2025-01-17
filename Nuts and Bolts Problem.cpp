437f6bd447e6ac8f08256be51fcd6c8522792007
DikshantBadawadagi <153322418+DikshantBadawadagi@users.noreply.github.com>
2025-01-17 23:59:32 +0530

    Create Nuts and Bolts Problem.cpp

---
+++ Nuts and Bolts Problem.cpp
@@ -0,0 +1,22 @@
+// User function template for C++
+class Solution {
+  public:
+
+   
+void matchPairs(int n, char nuts[], char bolts[]) 
+    {
+        char order[] = { '!', '#', '$', '%', '&', '*', '?', '@', '^'};
+        int pos = 0;
+        for(int i = 0; i < 9; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(order[i] == bolts[j])
+                {
+                    swap(bolts[j], bolts[pos]);
+                    nuts[pos] = bolts[pos++];
+                }
+            }
+        }
+    }
+};
